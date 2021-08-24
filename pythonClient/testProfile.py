2bcea3b893c4ffbad26d302fe99bed7ab07c9f5b
Marshall Conover <marzhall.o@gmail.com>
2021-08-23 20:47:34 -0400

    Make it so that users can set their client credentials via environment variable.

--- pythonClient/testProfile.py
+++ pythonClient/testProfile.py
@@ -1,19 +1,31 @@
 import requests
+import os
 from requests.api import head
 from requests.auth import HTTPBasicAuth
 from flask import render_template
 
 # THESE TWO VALUES will be provided to you as part
 # of your subless account. See: readme.md
+# You can also use the environment variables
+# CLIENT_ID and CLIENT_SECRET
 clientId = 'Your client ID';
 clientSecret = 'Your client secret';
-
 
 username = 'TestUser';
 sublessPaymentsUrl = 'https://pay.subless.com';
 sublessAuthUrl = 'https://subless-test.auth.us-east-1.amazoncognito.com';
 
 def GetAClientCredentialsToken():
+    global clientSecret
+    global clientId
+    if clientId == 'Your client ID':
+        clientId = os.getenv("CLIENT_ID")
+
+    if clientSecret == 'Your client secret':
+        clientSecret = os.getenv("CLIENT_SECRET")
+
+    print("clientid is " + str(clientId))
+    print("clientsecret is " + str(clientSecret))
     # Request a client credentials token
     r = requests.post(sublessAuthUrl + "/oauth2/token",\
         auth = HTTPBasicAuth(clientId, clientSecret),
