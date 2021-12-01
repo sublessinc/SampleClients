5ac348b8458bac66fe03b67c2e7b46c865177bc8
jon blanton <jon@subless.com>
2021-12-01 15:38:25 -0500

    Added an example of receiving creator activation updates via webhook

--- pythonClient/examplePythonClient.py
+++ pythonClient/examplePythonClient.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 from flask.globals import current_app
 import testProfile
+import json
 
 app = Flask(__name__)
 
@@ -11,6 +12,17 @@ def index():
 @app.route("/profile/TestUser")
 def TestUser():
     return testProfile.GenerateUserProfile()
+
+@app.route('/webhook', methods=['POST'])
+def creatorStatusWebhook():
+    returnedCreator = json.loads(request.data)
+    print(returnedCreator)
+    print("Creator Email: " + returnedCreator['email'], flush=True)
+    print("Creator Username: " + returnedCreator['username'], flush=True)
+    print("Creator Status: " + str(returnedCreator['active']), flush=True)
+    print("Creator Has been deleted: " + str(returnedCreator['isDeleted']), flush=True)
+    print("Creator Subless Id: " + returnedCreator['id'], flush=True)
+    return "Ok"
 
 @app.errorhandler(Exception)
 def HandleException(e):
