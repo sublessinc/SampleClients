1189a4c1a73a3f02051b083d37aae98b77b85003
Jonathan Blanton <jon36992002@gmail.com>
2021-12-19 17:15:18 -0500

    Feature/add webhook sample (#18)
    * Added an example of receiving creator activation updates via webhook
    
    * Added webhook sample
    
    * Added doc
    
    Co-authored-by: jon blanton <jon@subless.com>

--- pythonClient/examplePythonClient.py
+++ pythonClient/examplePythonClient.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 from flask.globals import current_app
 import testProfile
+import json
 
 app = Flask(__name__)
 
@@ -11,6 +12,20 @@ def index():
 @app.route("/profile/TestUser")
 def TestUser():
     return testProfile.GenerateUserProfile()
+
+# To be notified when creators link their account for your site to subless, set up a webhook to recieve a post call.
+# Once you've set up and deployed your API, register the URI on your subless partner account page.
+@app.route('/webhook', methods=['POST'])
+def creatorStatusWebhook():
+    # A Json object will be sent with the properties below
+    returnedCreator = json.loads(request.data)
+    print(returnedCreator)
+    print("Creator Email: " + returnedCreator['email'], flush=True)
+    print("Creator Username: " + returnedCreator['username'], flush=True)
+    print("Creator Status: " + str(returnedCreator['active']), flush=True)
+    print("Creator Has been deleted: " + str(returnedCreator['isDeleted']), flush=True)
+    print("Creator Subless Id: " + returnedCreator['id'], flush=True)
+    return "Ok"
 
 @app.errorhandler(Exception)
 def HandleException(e):
